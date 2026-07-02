23d16f6d2c6202f8125d2583ce974aeb6b086e8e
Piyush Sharma <piyushsharma50555055@gmail.com>
2026-07-02 21:32:59 +0530

    Added new Java code

---
+++ ContinueEx2.java
@@ -0,0 +1,12 @@
+public class ContinueEx2 {
+	public static void main(String[] args) {
+		for(int a=1;a<=5;a++) {
+			System.out.println("Piyush");
+			if(a==3){
+				continue;
+			}
+			System.out.println("Sharma");
+		}
+		System.out.println("Bye");
+	}
+}
